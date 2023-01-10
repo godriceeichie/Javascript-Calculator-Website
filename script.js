606862bf74eb5c8bb1d5947a204ab9fa0bd166a5
godriceeichie <emmaeichie@gmail.com>
2023-01-10 11:02:36 +0100

    Made the subtraction button to function

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function addSpace(word){
 let output1 = document.querySelector(".output1");
 let output2 = document.querySelector("#output2");
 let inputBtn = document.querySelectorAll(".input-btn");
-output2.value = '';
+output2.value = '0';
 inputBtn.forEach(box => {
     box.addEventListener("click", (e) =>{
         for(i = 0; i < inputBtn.length; i++){
@@ -26,11 +26,11 @@ inputBtn.forEach(box => {
         }
         
         if(!box.classList.contains('delete') && !box.classList.contains('equal') &&!box.classList.contains('clear')){
+            if(output2.value === '0'){
+                output2.value = '';
+            }
             output2.value += box.textContent
             addSpace(output2.value)
-        }
-        if(e.target.matches(".equal") && output2.value.includes("+")){
-            output2.value = add(output2.value)
         }
         
         if(e.target.matches(".delete")){
@@ -40,7 +40,14 @@ inputBtn.forEach(box => {
         if(e.target.matches(".clear")){
             output2.value = removeWord(output2.value)
         }
+
+        if(e.target.matches(".equal") && output2.value.includes("+")){
+            output2.value = add(output2.value)
+        }
         
+        if(e.target.matches(".equal") && output2.value.includes("−")){
+            output2.value = subtract(output2.value)
+        }
     })
     document.addEventListener("mousedown", function(e) {
         if (e.target !== box) {
@@ -76,3 +83,22 @@ function add(input){
         }
     }
 }
+
+function subtract(input){
+    let inputArray = input.split("−")
+    for(i = 0; i < inputArray.length; i++){
+        let letter = inputArray[i]
+        let difference = Number(letter)
+        for(j = i + 1; j < inputArray.length; j++){
+            let secondLetter = inputArray[j]
+            difference -= Number(secondLetter)
+        }
+        if(Number.isInteger(difference)){
+            return parseInt(difference)
+        }
+        else{
+            return parseFloat(difference).toFixed(6)
+        }
+    }
+}
+
