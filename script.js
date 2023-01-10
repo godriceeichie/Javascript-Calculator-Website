f504f94d9af6d57a4f74de9307252c35c5608a9b
godriceeichie <emmaeichie@gmail.com>
2023-01-10 06:15:07 +0100

    Made the add button to work

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 function addSpace(word){
-    return word.split("").join(" ")
+    return word.split("").join(" ");
 }
 let output1 = document.querySelector(".output1");
 let output2 = document.querySelector("#output2");
 let inputBtn = document.querySelectorAll(".input-btn");
 output2.value = '';
-addSpace(output2.value)
 inputBtn.forEach(box => {
     
     box.addEventListener("click", (e) =>{
         if(!box.classList.contains('delete') && !box.classList.contains('equal')){
             output2.value += box.textContent
+            addSpace(output2.value)
         }
-        if(e.target.matches(".equal")){
-            calc()
+        if(e.target.matches(".equal") && output2.value.includes("+")){
+            output2.value = add(output2.value)
         }
         
         if(e.target.matches(".delete")){
             output2.value = removeCharacter(output2.value)
         }
-        // console.log(e);
+        
     })
 })
 
@@ -28,22 +28,15 @@ function removeCharacter(word){
     return word
 }
 
-function calc(){
-    let number = "1234567890"
-    let outputItem = output2.value.split("");
-    // for(i = 0; i < outputItem.length; i++){
-    //     let firstItem = outputItem[i];
-    //     for(j = i + 1; outputItem.length; j++){
-    //         let secondItem = outputItem[j]
-    //         if(number.includes(firstItem) && number.includes(secondItem)){
-    //             let firstNumber = Number(firstItem)
-    //             let secondNumber = Number(secondItem)
-    //             output2.value = firstNumber + secondNumber;
-    //             console.log(typeof firstNumber);
-    //         }
-    //     }
-        
-        
-    // }
-    
+function add(input){
+    let inputArray = input.split("+")
+    for(i = 0; i < inputArray.length; i++){
+        let letter = inputArray[i]
+        let sum = parseInt(letter)
+        for(j = i + 1; j < inputArray.length; j++){
+            let secondLetter = inputArray[j]
+            sum += parseInt(secondLetter)
+        }
+        return sum
+    }
 }
